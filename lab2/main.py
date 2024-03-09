7cbeeeae308408b8ac2f8f45e9b13801d080f284
Klubni4ka193 <147038384+Klubni4ka193@users.noreply.github.com>
2024-03-09 20:09:23 +0300

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,9 +1,8 @@
-#Задание 16 №2
-
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
 
 # Параметры системы
 l = 1.0  # длина стержня и пружины
@@ -26,19 +25,22 @@ t = np.linspace(0, 10, 100)
 # Решение дифференциального уравнения
 sol = odeint(system, y0, t)
 
-# Функция для анимации движения
-fig, ax = plt.subplots()
-ax.set_xlim(-2, 2)
-ax.set_ylim(-2, 2)
-line, = ax.plot([], [], 'o-')
+# Функция для анимации движения в трехмерном пространстве
+fig = plt.figure(figsize=(10,8))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim([-2, 2])
+ax.set_ylim([-2, 2])
+ax.set_zlim([-2, 2])
+line, = ax.plot([], [], [], 'o-')
 
 def animate(i):
     x = np.array([0, l*np.sin(sol[i, 0]), l*np.sin(sol[i, 0])])
     y = np.array([0, -l*np.cos(sol[i, 0]), -l*np.cos(sol[i, 0])])
+    z = np.array([0, 0, 0])
     line.set_data(x, y)
+    line.set_3d_properties(z)
     return line,
 
 ani = animation.FuncAnimation(fig, animate, frames=len(sol), interval=50, blit=True)
-
 
 plt.show()
